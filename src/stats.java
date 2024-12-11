f052a2c99d50959ec5f942d6b4342d51f4e2fedb
danielnull <556862@my.cuhsd.org>
2024-12-11 09:18:29 -0800

    kghjhjg

--- src/stats.java
+++ src/stats.java
@@ -1,7 +1,7 @@
 public class stats {
 
     //returns time it took to type the prompt
-
+//asdasdasd
 
     //return how many errors you made, maybe where the errors. //calculate them as a %, show as accuracy
 
